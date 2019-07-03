3231dfca37348159818dec802a521ed583d4e157
cuongcb <cuongcb@vng.com.vn>
2019-07-03 22:14:25 +0700

    Get top ten posts from /r/subreddit

--- main.go
+++ main.go
@@ -1,58 +1,35 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"log"
-	"net/http"
-	"regexp"
-	"strings"
+
+	"github.com/turnage/graw/reddit"
 )
 
-func stripchars(str, chr string) string {
-	return strings.Map(func(r rune) rune {
-		if strings.IndexRune(chr, r) < 0 {
-			return r
-		}
-		return -1
-	}, str)
-}
-
-func handleError(e error) {
-	if e != nil {
-		log.Fatal(e)
-	}
-}
-
 func main() {
-	fmt.Println("Hello World.")
-	reg, err := regexp.Compile("https?://imgur.com/a......")
-	handleError(err)
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://www.reddit.com/r/programming/comments/c7nu7u.json?sort=best&limit=10&depth=10", nil)
-	req.Header.Set("User-Agent", "linux:go-postgrabber:v0.1")
-	handleError(err)
-
-	resp, err := client.Do(req)
+	bot, err := reddit.NewBotFromAgentFile("rf.env", 0)
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		defer resp.Body.Close()
-
-		buf := new(bytes.Buffer)
-
-		buf.ReadFrom(resp.Body)
-
-		s := buf.String() // Does a complete copy of the bytes in the buffer.
-
-		links := reg.FindString(stripchars(s, "\""))
-
-		fmt.Printf("%v\n", links)
-
-		fmt.Printf("%v\n", s)
+		fmt.Println("Failed to create bot handle: ", err)
+		return
 	}
 
-	fmt.Println(resp.Status)
-	resp.Body.Close()
+	options := map[string]string{
+		"limit": "10",
+		"sort":  "best",
+		"depth": "10",
+	}
+
+	harvest, err := bot.ListingWithParams("/r/golang",
+		options)
+	if err != nil {
+		fmt.Println("Failed to fetch /r/golang: ", err)
+		return
+	}
+
+	for index, post := range harvest.Posts {
+		fmt.Printf("Post [%d] *<%s>*\n", index, post.Title)
+		for _, comment := range post.Replies {
+			fmt.Printf("*** [%s] commented --> %s\n", comment.Author, comment.Body)
+		}
+	}
 }
